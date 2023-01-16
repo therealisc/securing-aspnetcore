1f0702cf726fdfd79a6ff5b24d6f16c47091bf0c
therealisc <ioan.scafa@gmail.com>
2023-01-16 16:24:46 +0200

    Secure access to the api

--- ImageGallery.API/Program.cs
+++ ImageGallery.API/Program.cs
@@ -1,5 +1,7 @@
+using System.IdentityModel.Tokens.Jwt;
 using ImageGallery.API.DbContexts;
 using ImageGallery.API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,6 +23,21 @@ builder.Services.AddScoped<IGalleryRepository, GalleryRepository>();
 // register AutoMapper-related services
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = "https://localhost:5001";
+        options.Audience = "imagegalleryapi";
+        options.TokenValidationParameters = new () 
+        {
+            NameClaimType = "given_name",
+            RoleClaimType = "role",
+            ValidTypes = new [] { "at+jwt" }
+        };
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -29,6 +46,7 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
